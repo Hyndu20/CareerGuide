4e24142ff72974063db9572d3ee036972f3c9dda
Hyndhu <hyndavi.ambati@iiitb.ac.in>
2023-05-13 00:48:06 +0530

    playbook

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const adminRoutes = require('./routes/admin')
 const userRoutes = require('./routes/user')
+const winston = require('winston');
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors');
@@ -12,8 +13,20 @@ const port = 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+  
 
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'logs/access.log' })
+  ]
+});
 app.use((req, res, next) => {
+    logger.info(`${req.method},${req.path} - ${req.ip}`);
     console.log(req.path, req.method);
     next();
 })
@@ -30,7 +43,7 @@ app.use('/admin', adminRoutes)
 app.use('/user', userRoutes)
 
 
-
+//mongoose.set('strictQuery', false);
 mongoose.connect('mongodb+srv://test:test@cluster0.l69p7lc.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: false,  useUnifiedTopology: false })
     .then(() => {
 
